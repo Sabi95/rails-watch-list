7bd38e920c6fe7af30e5e96bc35fc85e0ce08672
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-10 19:40:51 +0900

    movie list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,10 @@
 class BookmarksController < ApplicationController
 
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
